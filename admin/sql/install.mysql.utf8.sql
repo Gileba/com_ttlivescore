7563e94f8fbeb0e9015cff95deb0ea71781a9d1f
Gileba <gileba@macbook.local>
2017-01-23 23:01:27 +0100

    SQL

---
+++ admin/sql/install.mysql.utf8.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `#__ttlivescore_players` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT, 
+	`firstname` varchar(250) NOT NULL DEFAULT ``,
+	`lastname` varchar(250) NOT NULL DEFAULT ``,
+	PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
