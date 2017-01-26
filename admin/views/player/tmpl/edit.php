175167dc482eba62a6e35e573afc113d6e2c123c
Gileba <gileba@macbook.local>
2017-01-26 14:48:07 +0100

    New rendering of fields

--- admin/views/player/tmpl/edit.php
+++ admin/views/player/tmpl/edit.php
@@ -10,10 +10,8 @@
 				<?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
 				<?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_TTLIVESCORE_NEW_PLAYER', true) : JText::sprintf('COM_TTLIVESCORE_EDIT_PLAYER', $this->item->id, true)); ?>
 				<div class="control-group">
-					<div class="control-label"><?php echo $this->form->getLabel('lastname'); ?></div>
-					<div class="controls"><?php echo $this->form->getInput('lastname'); ?></div>
-					<div class="control-label"><?php echo $this->form->getLabel('firstname'); ?></div>
-					<div class="controls"><?php echo $this->form->getInput('firstname'); ?></div>
+					<?php echo $this->form->renderField('lastname'); ?>
+					<?php echo $this->form->renderField('firstname'); ?>
 				</div>
 				<?php echo JHtml::_('bootstrap.endPanel'); ?>
 				<input type="hidden" name="task" value="" />
