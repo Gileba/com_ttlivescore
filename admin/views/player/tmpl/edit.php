f86e5c1689853f87572fa45cf6f04cc93ac0d992
Gileba <gileba@macbook.local>
2017-01-27 08:41:26 +0100

    New fields

--- admin/views/player/tmpl/edit.php
+++ admin/views/player/tmpl/edit.php
@@ -10,7 +10,11 @@
 				<?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
 				<?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_TTLIVESCORE_NEW_PLAYER', true) : JText::sprintf('COM_TTLIVESCORE_EDIT_PLAYER', $this->item->id, true)); ?>
 				<?php echo $this->form->renderField('lastname'); ?>
+				<?php echo $this->form->renderField('middlename'); ?>
 				<?php echo $this->form->renderField('firstname'); ?>
+				<?php echo $this->form->renderField('published'); ?>
+				<?php echo $this->form->renderField('image'); ?>
+				<?php echo $this->form->renderField('country'); ?>
 				<?php echo JHtml::_('bootstrap.endPanel'); ?>
 				<input type="hidden" name="task" value="" />
 				<?php echo JHtml::_('form.token'); ?>
