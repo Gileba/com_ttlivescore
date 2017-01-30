2acc2b36f5b60362e99e3ce347fb08f6f6a02f89
Gileba <gileba@macbook.local>
2017-01-30 08:44:56 +0100

    Ordering according to the book

--- admin/models/players.php
+++ admin/models/players.php
@@ -12,14 +12,22 @@
 			parent::__construct($config);
 		}
 		
+		protected function populateState($ordering = null, $direction = null)
+		{
+				parent::populateState('a.lastname', 'asc')
+		}
+			
 		protected function getListQuery()
 		{
 			$db	= $this->getDbo();
 			$query = $db->getQuery(true);
+			$orderCol = $this->state->get('list.ordering');
+			$orderDirn = $this->state->get('list.directiion');
 			
 			$query
 				->select($db->quoteName(array('a.id', 'a.lastname', 'a.firstname', 'a.middlename', 'a.country', 'a.published', 'a.dateofbirth', 'a.image', )))
 				->from($db->quoteName('#__ttlivescore_players', 'a'));
+				->order($db->escape($orderCol . ' ' . $orderDirn));
 			
 			return $query;
 		}
