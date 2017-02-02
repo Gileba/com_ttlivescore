b8ad1c011fe98a43f86d2a00bea6e1a643391f86
Gileba <gileba@macbook.local>
2017-02-02 23:29:37 +0100

    Pagination

--- admin/views/players/view.html.php
+++ admin/views/players/view.html.php
@@ -8,9 +8,10 @@
 		protected $pagination;
 		
 		public function display($tpl = null) {
-			$this->items = $this->get('Items');
-			$this->state = $this->get('State');
-			
+			$this->items 		= $this->get('Items');
+			$this->state 		= $this->get('State');
+			$this->pagination	= $this->get('Pagination');
+						
 			if (count($errors = $this->get('Errors')))
 			{
 				JError::raiseError(500, implode("/n", $errors));
