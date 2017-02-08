3d8d6f81f2071b0e324f697f3559171b6e8db7dd
Gileba <gileba@macbook.local>
2017-02-08 11:12:53 +0100

    Typo in SQL

--- admin/sql/updates/mysql/0.2.3.sql
+++ admin/sql/updates/mysql/0.2.3.sql
@@ -1,3 +1,3 @@
 # SQL-file for the 0.2.3 update
-ALTER TABLE `#__ttlivescore_countries` DROP column 'order';
-ALTER TABLE `#__ttlivescore_countries` ADD column 'ordering' int(3) NOT NULL DEFAULT '1';
+ALTER TABLE `#__ttlivescore_countries` DROP column `order`;
+ALTER TABLE `#__ttlivescore_countries` ADD column `ordering` int(3) NOT NULL DEFAULT '1';
