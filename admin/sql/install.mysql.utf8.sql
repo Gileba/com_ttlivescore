4d543d8a24d4346fd699e5199ce4af90bd5b9542
Gileba <gileba@macbook.local>
2017-02-19 22:17:00 +0100

    Alpha 2

--- admin/sql/install.mysql.utf8.sql
+++ admin/sql/install.mysql.utf8.sql
@@ -82,11 +82,11 @@ CREATE TABLE IF NOT EXISTS `#__ttlivescore_matchdefinitions` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 CREATE TABLE IF NOT EXISTS `#__ttlivescore_clubmatches` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT, 
-	`sid` int(10) unsigned NOT NULL DEFAULT '', 
-	`mdid` int(10) unsigned NOT NULL DEFAULT '', 
+	`sid` int(10) unsigned NOT NULL DEFAULT '0', 
+	`mdid` int(10) unsigned NOT NULL DEFAULT '0', 
 	`date` datetime NOT NULL default '0000-00-00 00:00:00',
-	`homeclub` int(10) unsigned NOT NULL DEFAULT '',
-	`awayclub` int(10) unsigned NOT NULL DEFAULT '',
+	`homeclub` int(10) unsigned NOT NULL DEFAULT '0',
+	`awayclub` int(10) unsigned NOT NULL DEFAULT '0',
 	`homeplayers` varchar(255) NOT NULL DEFAULT '',
 	`awayplayers` varchar(255) NOT NULL DEFAULT '',
 	`homereserves` varchar(255) NOT NULL DEFAULT '',
--- admin/sql/updates/mysql/0.4.1-alpha.2.sql
+++ admin/sql/updates/mysql/0.4.1-alpha.2.sql
@@ -1,11 +1,11 @@
 # SQL-file for the 0.4.1 update
 CREATE TABLE IF NOT EXISTS `#__ttlivescore_clubmatches` (
 	`id` int(10) unsigned NOT NULL AUTO_INCREMENT, 
-	`mdid` int(10) unsigned NOT NULL DEFAULT '', 
-	`sid` int(10) unsigned NOT NULL DEFAULT '', 
+	`sid` int(10) unsigned NOT NULL DEFAULT '0', 
+	`mdid` int(10) unsigned NOT NULL DEFAULT '0', 
 	`date` datetime NOT NULL default '0000-00-00 00:00:00',
-	`homeclub` int(10) unsigned NOT NULL DEFAULT '',
-	`awayclub` int(10) unsigned NOT NULL DEFAULT '',
+	`homeclub` int(10) unsigned NOT NULL DEFAULT '0',
+	`awayclub` int(10) unsigned NOT NULL DEFAULT '0',
 	`homeplayers` varchar(255) NOT NULL DEFAULT '',
 	`awayplayers` varchar(255) NOT NULL DEFAULT '',
 	`homereserves` varchar(255) NOT NULL DEFAULT '',
