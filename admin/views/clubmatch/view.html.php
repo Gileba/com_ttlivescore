f96c188d6484bdf5529d22f38bad5f8a856fbc11
Gileba <gileba@macbook.local>
2017-02-24 22:46:38 +0100

    missing ')'

--- admin/views/clubmatch/view.html.php
+++ admin/views/clubmatch/view.html.php
@@ -41,7 +41,7 @@
 			
 			if($this->item->livescorescreated == false)
 			{
-				JToolbarHelper::custom('clubmatch.creatematches', 'tree-2', '', JText::_('COM_TTLIVESCORE_BUTTON_CREATEMATCHES', false);
+				JToolbarHelper::custom('clubmatch.creatematches', 'tree-2', '', JText::_('COM_TTLIVESCORE_BUTTON_CREATEMATCHES'), false);
 			}
 		}
 	}
