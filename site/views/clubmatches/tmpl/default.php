4c2981a15e9ece807aa56dfa2d8096e38ce55a2a
Gileba <gileba@macbook.local>
2017-03-06 14:29:53 +0100

    Correction dbfield

--- site/views/clubmatches/tmpl/default.php
+++ site/views/clubmatches/tmpl/default.php
@@ -6,7 +6,7 @@
 	<?php foreach ($this->items as $item) : ?>
 		<div class="clubmatch">
 			<div class="clubs">
-				<?php echo $item->homeclubid . ' - ' . $item->awayclubid; ?>
+				<?php echo $item->homeclub . ' - ' . $item->awayclub; ?>
 			</div>
 		</div>
 	<?php endforeach; ?>
