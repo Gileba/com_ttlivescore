1566ea562bedc9453371eafeb1d7d47e1e77dfa0
Gileba <gileba@macbook.local>
2017-03-07 23:07:28 +0100

    Show score on overview

--- site/views/clubmatches/tmpl/default.php
+++ site/views/clubmatches/tmpl/default.php
@@ -1,12 +1,14 @@
 <?php
 	defined('_JEXEC') or die;
+	
+	$model = $this->getModel();
 ?>
 
 <div class="clubmatches">
 	<?php foreach ($this->items as $item) : ?>
 		<div class="clubmatch">
 			<div class="clubs">
-				<?php echo $item->homeclub . ' - ' . $item->awayclub; ?>
+				<?php echo $item->homeclub . ' - ' . $item->awayclub . '(' . $model->getScore($item->id) . ')'; ?>
 			</div>
 		</div>
 	<?php endforeach; ?>
