101afeb6643ec285d163e53bc52b64d8868b9b57
Gileba <info@gileba.be>
2017-07-22 23:52:41 +0200

    Do noet load livescore class twice
    Closes #246

--- site/views/clubmatch/tmpl/default.php
+++ site/views/clubmatch/tmpl/default.php
@@ -14,11 +14,12 @@
 	setInterval(function () { loadLivescore() },30000);
 
 	function loadLivescore() {
-		jQuery( "#livescore" ).load( "<?php echo JUri::getInstance(); ?> #livescore" );
+		jQuery( "#livescore-wrapper" ).load( "<?php echo JUri::getInstance(); ?> .livescore" );
 	}
 </script>
 
-<div id="livescore">
+<div id="livescore-wrapper">
+	<div class="livescore">
 		<div class="clubmatch">
 			<div class="home">
 				<div class="club">
@@ -94,4 +95,5 @@
 			</div>
 		</div>
 	<?php endforeach; ?>
+	</div>
 </div>
