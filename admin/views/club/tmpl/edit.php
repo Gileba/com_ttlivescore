3a67d46b2edd05a1e7ae26bc7009f147ca7dadf2
Gileba <info@gileba.be>
2017-08-16 21:19:28 +0200

    Extra safety measure by typecasting int

--- admin/views/club/tmpl/edit.php
+++ admin/views/club/tmpl/edit.php
@@ -8,7 +8,7 @@
 		<div class="span10 form-horizontal">
 			<fieldset>
 				<?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
-				<?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_TTLIVESCORE_NEW_CLUB', true) : JText::sprintf('COM_TTLIVESCORE_EDIT_CLUB', $this->item->id, true)); ?>
+				<?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_TTLIVESCORE_NEW_CLUB', true) : JText::sprintf('COM_TTLIVESCORE_EDIT_CLUB', (int) $this->item->id, true)); ?>
 				<?php echo $this->form->renderField('name'); ?>
 				<?php echo $this->form->renderField('published'); ?>
 				<?php echo $this->form->renderField('emblem'); ?>
