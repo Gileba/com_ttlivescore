d2f9387bcb2afed55d0eba0170b221e5792424ad
Gileba <Gileba@users.noreply.github.com>
2017-08-24 17:42:37 +0200

    Add function to go to season details

--- admin/controllers/season.php
+++ admin/controllers/season.php
@@ -2,5 +2,17 @@
 	defined('_JEXEC') or die;
 	class TTLivescoreControllerSeason extends JControllerForm 
 	{
-		
+		public function details()
+		{
+			// Save form
+			$this->save();
+
+			// Get the input
+			$input = JFactory::getApplication()->input;
+			$id = $input->getInt('id', '0');
+			
+			// Redirect to the season details form.
+			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=seasondetails&layout=edit&id=' . (int) $id, false));
+			$this->redirect();
+		}		
 	}
