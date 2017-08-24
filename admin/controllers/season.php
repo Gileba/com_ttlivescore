10d7f3eb30d9d56a25e906d5d10b2cdc88046510
Gileba <Gileba@users.noreply.github.com>
2017-08-24 17:46:28 +0200

    Redirect to complete list

--- admin/controllers/season.php
+++ admin/controllers/season.php
@@ -4,15 +4,12 @@
 	{
 		public function details()
 		{
-			// Save form
-			$this->save();
-
 			// Get the input
 			$input = JFactory::getApplication()->input;
 			$id = $input->getInt('id', '0');
 			
 			// Redirect to the season details form.
-			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=seasondetails&layout=edit&id=' . (int) $id, false));
+			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=seasondetails';
 			$this->redirect();
 		}		
 	}
