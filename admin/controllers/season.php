0c6db21d97cb9816fd92aa41e997abd50b4c0b0f
Gileba <Gileba@users.noreply.github.com>
2017-08-25 17:21:03 +0200

    Correct transfer

--- admin/controllers/season.php
+++ admin/controllers/season.php
@@ -5,10 +5,14 @@
 		public function details()
 		{
 			// Get the input
-			$id = JFactory::getApplication()->input->getInt('id', '0');
+			$app = JFactory::getApplication();
+			$id = $app->input->getInt('id', '0');
+			
+			// Set filter for Season Details
+			$app->setUserState('com_ttlivescore.seasondetails.filter.seasons', $id );
 			
 			// Redirect to the season details form.
-			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=seasondetails&season=' . (int) $id), false);
+			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=seasondetails', false));
 			$this->redirect();
 		}		
 	}
