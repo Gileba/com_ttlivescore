b2337b346b7b797b449023c8305c2597b17df47b
Gileba <Gileba@users.noreply.github.com>
2017-09-10 22:40:44 +0200

    Fix SQL syntax

--- admin/sql/updates/mysql/0.7.5.sql
+++ admin/sql/updates/mysql/0.7.5.sql
@@ -1,3 +1,3 @@
 # SQL-file for the 0.7.5 update
 ALTER TABLE `#__ttlivescore_players`
-ALTER COLUMN `dateofbirth` date DEFAULT '0000-00-00 00:00:00';
+ALTER COLUMN `dateofbirth` SET DEFAULT '0000-00-00 00:00:00';
