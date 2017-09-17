bb108e43113062f38d2692a83f021dc0cc31804f
Gijs Lamon <Gileba@users.noreply.github.com>
2017-09-17 21:25:30 +0200

    Add countries needed for Dutch Eredivisie (#383)

---
+++ admin/sql/updates/mysql/0.7.6.sql
@@ -0,0 +1,5 @@
+# SQL-file for the 0.7.6 update
+INSERT INTO `#__ttlivescore_countries` (name,ioc_code) VALUES ('COM_TTLIVESCORE_COUNTRY_DENMARK', 'DEN');
+INSERT INTO `#__ttlivescore_countries` (name,ioc_code) VALUES ('COM_TTLIVESCORE_COUNTRY_GERMANY', 'GER');
+INSERT INTO `#__ttlivescore_countries` (name,ioc_code) VALUES ('COM_TTLIVESCORE_COUNTRY_IRAN', 'IRI');
+INSERT INTO `#__ttlivescore_countries` (name,ioc_code) VALUES ('COM_TTLIVESCORE_COUNTRY_PORTUGAL', 'POR');
