352345957e21fcc44241b14b9e2d59c9327219b7
Gileba <Gileba@users.noreply.github.com>
2017-09-19 17:55:28 +0200

    Basic start

--- admin/controllers/livescore.php
+++ admin/controllers/livescore.php
@@ -3,5 +3,20 @@
 
 	class TTLivescoreControllerLivescore extends JControllerForm 
 	{
+		public function nextlivescore()
+		{
+			// Get the input
+			$input = JFactory::getApplication()->input;
+			$id = $input->getInt('id', '0');
+ 
+			// Get the model
+			$model = $this->getModel();
+			
+			if ($id !== 0) {
+ 
+			}
 
+			$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=livescores&id=' . (int) $model->cmid, false));
+			$this->redirect();
+		}
 	}
