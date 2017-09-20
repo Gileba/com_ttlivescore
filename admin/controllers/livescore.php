3b448e917586acbc0c01bd68e60746535b397a8c
Gileba <Gileba@users.noreply.github.com>
2017-09-20 19:13:44 +0200

    Add layout

--- admin/controllers/livescore.php
+++ admin/controllers/livescore.php
@@ -15,7 +15,7 @@
 			
 			if ($nextMatch->matchid !== 0) {
 			// Redirect
-				$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=livescore&id=' . (int) $nextMatch->matchid, false));
+				$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=livescore&layout=edit&id=' . (int) $nextMatch->matchid, false));
 				$this->redirect();				
 			}
 			
