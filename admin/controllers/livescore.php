c4c6d65ef35b55138edf3994b1c32bf14bee4f94
Gileba <Gileba@users.noreply.github.com>
2017-09-22 17:36:33 +0200

    Correct context

--- admin/controllers/livescore.php
+++ admin/controllers/livescore.php
@@ -16,18 +16,16 @@
 			if ($nextMatch['matchid'] !== 0) {
 				// Give user edit access
 				$app = JFactory::getApplication();
-				$values = (array) $app->getUserState('$this->option.edit.$this->context');
+				$context = $this->option . '.edit.' . $this->context . '.id';
+				$values = (array) $app->getUserState($context);
 
 				// Add the id to the list if non-zero.
 				if (!empty($nextMatch['matchid']))
 				{
 					$values[] = (int) $nextMatch['matchid'];
 					$values   = array_unique($values);
-					$context = $this->option . '.edit.' . $this->context . '.id';
-					JFactory::getApplication()->enqueueMessage($context);
 					$app->setUserState($context, $values);
 				}
-
 
 				// Redirect
 				$this->setRedirect(JRoute::_('index.php?option=com_ttlivescore&view=livescore&layout=edit&id=' . (int) $nextMatch['matchid'], false));
