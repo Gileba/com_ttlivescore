36ed54505f3beea6d52f35f6a80671760d8a5115
Gileba <Gileba@users.noreply.github.com>
2019-06-06 14:01:55 +0200

    Lines are too long

--- src/admin/views/countries/tmpl/default.php
+++ src/admin/views/countries/tmpl/default.php
@@ -50,8 +50,13 @@ if ($saveOrder)
 			/>
 		</div>
 		<div class="btn-group pull-left">
-			<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?> "><i class="icon-search"></i></button>
-			<button class="btn hasTooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.getElementById('filter_search').value=''; this.form.submit();"> <i class="icon-remove"> </i></button>
+			<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?> ">
+				<i class="icon-search"></i>
+			</button>
+			<button class="btn hasTooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>"
+				onclick="document.getElementById('filter_search').value=''; this.form.submit();">
+				<i class="icon-remove"> </i>
+			</button>
 		</div>
 		<div class="btn-group pull-right hidden-phone">
 			<label for="limit" class="element-invisible">
@@ -82,10 +87,15 @@ if ($saveOrder)
 			<thead>
 				<tr>
 					<th width="1%" class="nowrap center hidden-phone">
-						<?php echo JHtml::_('grid.sort', '<i class="icon-menu-2"></i>', 'a.ordering', $listDirn, $listOrder, null, 'asc', 'JGRID_HEADING_ORDERING'); ?>
+						<?php
+							echo JHtml::_('grid.sort', '<i class="icon-menu-2"></i>', 'a.ordering', $listDirn, $listOrder, null, 'asc',
+								'JGRID_HEADING_ORDERING'
+							);
+						?>
 					</th>
 					<th width="1%" class="hidden-phone">
-						<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+						<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>"
+							onclick="Joomla.checkAll(this)" />
 					</th>
 					<th width="1%" style="min-width: 55px;" class="nowrap center">
 						<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.published', $listDirn, $listOrder); ?>
@@ -107,7 +117,8 @@ if ($saveOrder)
 			</tfoot>
 			<tbody>
 				<?php foreach($this->items as $i => $item) :
-					$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out === $user->get('id') || $item->checked_out === 0;
+					$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out === $user->get('id')
+						|| $item->checked_out === 0;
 					$canChange = $user->authorise('core.edit.state', 'com_ttlivescore') && $canCheckin;
 					?>
 				<tr class="row<?php echo $i % 2; ?>" sortable-group-id="1">
@@ -124,7 +135,8 @@ if ($saveOrder)
 						<span class="sortable-handler hasTooltip <?php echo $disableClassName; ?>" title="<?php echo $disabledLabel; ?>">
 							<i class="icon-menu"></i>
 						</span>
-						<input type="text" style="display:none;" name="order[]" size="5" value="<?php echo $item->ordering; ?>" class="width-20 text-area-order" />
+						<input type="text" style="display:none;" name="order[]" size="5" value="<?php echo $item->ordering; ?>"
+							class="width-20 text-area-order" />
 					<?php else : ?>
 						<span class="sortable-handler inactive">
 							<i class="icon-menu"></i>
@@ -135,7 +147,10 @@ if ($saveOrder)
 						<?php echo JHtml::_('grid.id', $i, $item->id); ?>
 					</td>
 					<td class="center">
-						<?php echo JHtml::_('jgrid.published', $item->published, $i, 'countries.', $canChange, 'cb', $item->publish_up, $item->publish_down); ?>
+						<?php 
+							echo JHtml::_('jgrid.published', $item->published, $i, 'countries.', $canChange, 'cb', $item->publish_up,
+								$item->publish_down);
+						?>
 					</td>
 					<td class="nowrap has-context">
 						<a href="<?php echo JROUTE::_('index.php?option=com_ttlivescore&task=country.edit&id=' . (int) $item->id); ?>">
--- src/admin/views/players/tmpl/default.php
+++ src/admin/views/players/tmpl/default.php
@@ -44,8 +44,13 @@
 			/>
 		</div>
 		<div class="btn-group pull-left">
-			<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?> "><i class="icon-search"></i></button>
-			<button class="btn hasTooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>" onclick="document.getElementById('filter_search').value=''; this.form.submit();"> <i class="icon-remove"> </i></button>
+			<button class="btn hasTooltip" type="submit" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?> ">
+				<i class="icon-search"></i>
+			</button>
+			<button class="btn hasTooltip" type="button" title="<?php echo JText::_('JSEARCH_FILTER_CLEAR'); ?>"
+				onclick="document.getElementById('filter_search').value=''; this.form.submit();">
+				<i class="icon-remove"> </i>
+			</button>
 		</div>
 		<div class="btn-group pull-right hidden-phone">
 			<label for="limit" class="element-invisible">
@@ -76,7 +81,8 @@
 			<thead>
 				<tr>
 					<th width="1%" class="hidden-phone">
-						<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
+						<input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>"
+							onclick="Joomla.checkAll(this)" />
 					</th>
 					<th width="1%" style="min-width: 55px;" class="nowrap center">
 						<?php echo JHtml::_('grid.sort', 'JSTATUS', 'a.published', $listDirn, $listOrder); ?>
@@ -104,7 +110,8 @@
 			</tfoot>
 			<tbody>
 				<?php foreach($this->items as $i => $item) :
-					$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out === $user->get('id') || $item->checked_out === 0;
+					$canCheckin = $user->authorise('core.manage', 'com_checkin') || $item->checked_out === $user->get('id')
+						|| $item->checked_out === 0;
 					$canChange = $user->authorise('core.edit.state', 'com_ttlivescore') && $canCheckin;
 					?>
 				<tr class="row<?php echo $i % 2; ?>">
@@ -116,7 +123,9 @@
 							<?php
 							if ($canChange)
 								{
-								echo JHtml::_('jgrid.published', $item->published, $i, 'players.', $canChange, 'cb', $item->publish_up, $item->publish_down);
+								echo JHtml::_('jgrid.published', $item->published, $i, 'players.', $canChange, 'cb', $item->publish_up,
+									$item->publish_down
+								);
 								JHtml::_('actionsdropdown.' . ((int) $item->published === 2 ? 'un' : '') . 'archive', 'cb' . $i, 'players');
 								echo JHtml::_('actionsdropdown.render', $this->escape($item->lastname . ' ' . $item->firstname));
 							}
