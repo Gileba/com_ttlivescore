3269d460e0e6753d129e864f69a0be82d1a613b7
Gileba <Gileba@users.noreply.github.com>
2019-06-11 09:06:18 +0200

    Update clubmatches.php

--- src/site/models/clubmatches.php
+++ src/site/models/clubmatches.php
@@ -26,15 +26,18 @@ class TTLivescoreModelClubmatches extends JModelList
 		$query = $db->getQuery(true);
 
 		$query
-			->select($db->quoteName(array('a.id', 's.name', 'hc.name', 'ac.name', 'a.date', 'a.livescorescreated'),
-				array('id', 'season', 'homeclub', 'awayclub', 'matchdate', 'livescorescreated'))
+			->select(
+				$db->quoteName(
+					array('a.id', 's.name', 'hc.name', 'ac.name', 'a.date', 'a.livescorescreated'),
+					array('id', 'season', 'homeclub', 'awayclub', 'matchdate', 'livescorescreated')
 				)
+			)
 			->from($db->quoteName('#__ttlivescore_clubmatches', 'a'))
 			->join('INNER', $db->quoteName('#__ttlivescore_clubs', 'hc') . ' ON (' . $db->quoteName('a.homeclub') . ' = ' . $db->quoteName('hc.id') .
-			')'
+				')'
 			)
 			->join('INNER', $db->quoteName('#__ttlivescore_clubs', 'ac') . ' ON (' . $db->quoteName('a.awayclub') . ' = ' . $db->quoteName('ac.id') .
-			')'
+				')'
 			)
 			->join('INNER', $db->quoteName('#__ttlivescore_seasons', 's') . ' ON (' . $db->quoteName('a.sid') . ' = ' . $db->quoteName('s.id') . ')')
 			->where($db->quoteName('a.livescorescreated') . ' = 1')
