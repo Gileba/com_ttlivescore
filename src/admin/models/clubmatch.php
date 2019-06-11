28e9a19d730962594b0da9f7bde1b8c4c1921b4a
Gijs Lamon <Gileba@users.noreply.github.com>
2019-06-11 17:47:41 +0200

    Update clubmatch.php

--- src/admin/models/clubmatch.php
+++ src/admin/models/clubmatch.php
@@ -2,6 +2,11 @@
 	defined('_JEXEC') or die;
 class TTLivescoreModelClubmatch extends JModelAdmin
 {
+	/**
+	 * Model text prefix string.
+	 *
+	 * @var		string
+	 */
 	protected $text_prefix = 'COM_TTLIVESCORE';
 
 	public function getTable($type = 'Clubmatch', $prefix = 'TTLivescoreTable', $config = array())
