8ad11ba8be081a806f74f6d4fa82e07a52f639a5
Gileba <Gileba@users.noreply.github.com>
2019-06-11 09:05:09 +0200

    Update countries.php

--- src/admin/models/countries.php
+++ src/admin/models/countries.php
@@ -40,9 +40,11 @@ class TTLivescoreModelCountries extends JModelList
 		$orderDirn = $this->state->get('list.direction');
 
 		$query
-			->select($db->quoteName(array('a.id', 'a.name', 'a.ioc_code', 'a.published', 'a.ordering', 'a.publish_up', 'a.publish_down',
-				'a.rankingprefix'))
+			->select(
+				$db->quoteName(
+					array('a.id', 'a.name', 'a.ioc_code', 'a.published', 'a.ordering', 'a.publish_up', 'a.publish_down', 'a.rankingprefix')
 				)
+			)
 			->from($db->quoteName('#__ttlivescore_countries', 'a'))
 			->order($orderCol . ' ' . $orderDirn);
 
