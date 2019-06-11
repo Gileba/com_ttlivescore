1f8e8dff3d18e301d9422052869e6f1491258358
Gileba <Gileba@users.noreply.github.com>
2019-06-11 09:04:20 +0200

    Update livescores.php

--- src/admin/models/livescores.php
+++ src/admin/models/livescores.php
@@ -33,13 +33,19 @@ class TTLivescoreModelLivescores extends JModelList
 		$orderDirn = $this->state->get('list.direction');
 
 		$query
-			->select($db->quoteName(array('a.id', 'a.matchid', 'hp.lastname', 'hp.firstname', 'hp.middlename', 'ap.lastname', 'ap.firstname',
-				'ap.middlename', 'a.homepointsset1', 'a.homepointsset2', 'a.homepointsset3', 'a.homepointsset4', 'a.homepointsset5',
-				'a.homepointsset5', 'a.homepointsset7', 'a.awaypointsset1', 'a.awaypointsset2', 'a.awaypointsset3', 'a.awaypointsset4',
-				'a.awaypointsset5', 'a.awaypointsset6', 'a.awaypointsset7', 'md.sets'), array('id', 'matchid', 'homeplayerlastname',
-				'homeplayerfirstname', 'homeplayermiddlename', 'awayplayerlastname', 'awayplayerfirstname', 'awayplayermiddlename', 'homeset1',
-				'homeset2', 'homeset3', 'homeset4', 'homeset5', 'homeset6', 'homeset7', 'awayset1', 'awayset2', 'awayset3', 'awayset4', 'awayset5',
-				'awayset6', 'awayset7', 'numberofsets')))
+			->select(
+				$db->quoteName(
+					array('a.id', 'a.matchid', 'hp.lastname', 'hp.firstname', 'hp.middlename', 'ap.lastname', 'ap.firstname', 'ap.middlename',
+						'a.homepointsset1', 'a.homepointsset2', 'a.homepointsset3', 'a.homepointsset4', 'a.homepointsset5', 'a.homepointsset5',
+						'a.homepointsset7', 'a.awaypointsset1', 'a.awaypointsset2', 'a.awaypointsset3', 'a.awaypointsset4', 'a.awaypointsset5',
+						'a.awaypointsset6', 'a.awaypointsset7', 'md.sets'
+					),
+					array('id', 'matchid', 'homeplayerlastname', 'homeplayerfirstname', 'homeplayermiddlename', 'awayplayerlastname',
+						'awayplayerfirstname', 'awayplayermiddlename', 'homeset1', 'homeset2', 'homeset3', 'homeset4', 'homeset5', 'homeset6',
+						'homeset7', 'awayset1', 'awayset2', 'awayset3', 'awayset4', 'awayset5','awayset6', 'awayset7', 'numberofsets'
+					)
+				)
+			)
 			->from($db->quoteName('#__ttlivescore_livescores', 'a'))
 			->join('INNER', $db->quoteName('#__ttlivescore_players', 'hp') . ' ON (' . $db->quoteName('a.homeplayerid') . ' = ' .
 				$db->quoteName('hp.id') . ')'
